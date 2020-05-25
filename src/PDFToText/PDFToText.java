63f028ac196ef23b791618c058debb8600198907
Alex Enache <alexandruenache98@gmail.com>
2020-05-25 18:22:53 +0300

    added email finding

--- src/PDFToText/PDFToText.java
+++ src/PDFToText/PDFToText.java
@@ -1,33 +1,50 @@
 package PDFToText;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.*;
+import java.util.Optional;
+
 import PDFManager.PDFManager;
+import org.jetbrains.annotations.NotNull;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class PDFReader {
-    static BufferedWriter writer;
 
-    static {
-        try {
-            writer = new BufferedWriter(new FileWriter("C:\\Users\\Enche\\IdeaProjects\\PDF-Differential\\resources\\PDFToText\\pdfToText.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
+public class PDFToText {
+
+    protected static String listOfSeparators = " ,\n";
+    /*https://stackoverflow.com/questions/15703704/find-emails-in-a-string*/
+    protected static String emailRegex = "[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+";
+    protected static Pattern emailPatternRegex = Pattern.compile(emailRegex, Pattern.CASE_INSENSITIVE);
+    static void printToTxt(@NotNull PDFManager pdfManager, @NotNull PrintStream printStream) throws IOException {
+        String text = pdfManager.toText();
+        printStream.append(text);
+    }
+    //not using regex
+    static Optional<String> getFirstEmailFromPDFIfAny(@NotNull PDFManager pdfManager) throws IOException {
+        String content = pdfManager.toText();
+        StringBuilder email = new StringBuilder();
+        int indexOfEmailAt = content.indexOf('@');
+        if(indexOfEmailAt == -1) return Optional.empty();
+        for(int j = indexOfEmailAt - 1; j >= 0 && listOfSeparators.indexOf(content.charAt(j)) == -1; j--){
+            email.append(content.charAt(j));
         }
+        email = email.reverse();
+        email.append('@');
+        for(int j = indexOfEmailAt + 1; j < content.length() && listOfSeparators.indexOf(content.charAt(j)) == -1; j++){
+            email.append(content.charAt(j));
+        }
+        return Optional.of(email.toString());
     }
 
-    public static void main(String[] args) throws IOException {
-        PDFManager pdfManager = new PDFManager();
-        pdfManager.setFilePath("C:\\Users\\Enche\\IdeaProjects\\PDF-Differential\\resources\\PDFToText\\test.pdf");
-        try {
-            String text = pdfManager.toText();
-            writer.write(text);
-
-        } catch (IOException ex) {
-            Logger.getLogger(PDFReader.class.getName()).log(Level.SEVERE, null, ex);
+    static Optional<String> getEmailFromPDFIfAny(PDFManager pdfManager) throws IOException{
+        String content = pdfManager.toText();
+        StringBuilder email = new StringBuilder();
+        int indexOfEmailAt = content.indexOf('@');
+        if(indexOfEmailAt == -1) return Optional.empty();
+        Matcher matcher = emailPatternRegex.matcher(content);
+        while (matcher.find()) {
+            email.append(matcher.group());
         }
-        writer.close();
+        return Optional.of(email.toString());
     }
 }
--- src/PDFToText/PDFToTextTest.java
+++ src/PDFToText/PDFToTextTest.java
@@ -1,4 +1,38 @@
 package PDFToText;
 
+import PDFManager.PDFManager;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static PDFToText.PDFToText.*;
+
 public class PDFToTextTest {
+
+    protected static PrintStream writer;
+
+    public static void main(String[] args) throws IOException {
+        PDFManager pdfManager = new PDFManager();
+        pdfManager.setFilePath("..\\PDF-Differential\\resources\\PDFToText\\test.pdf");
+        try {
+            writer = new PrintStream(new File("..\\PDF-Differential\\resources\\PDFToText\\pdfToText.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        printToTxt(pdfManager, writer);
+        printToTxt(pdfManager, System.out);
+        System.out.println(getFirstEmailFromPDFIfAny(pdfManager).toString());
+        System.out.println(PDFToText.getEmailFromPDFIfAny(pdfManager).toString());
+
+        try {
+            writer = new PrintStream(new File("..\\PDF-Differential\\resources\\PDFToText\\listOfEmailOutput.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        pdfManager.setFilePath("..\\PDF-Differential\\resources\\PDFToText\\listOfEmails.pdf");
+        printToTxt(pdfManager, writer);
+
+    }
 }
