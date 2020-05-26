baa301d4d4d2f6c5abef87f9d0583847311cffae
Alex Enache <alexandruenache98@gmail.com>
2020-05-26 17:59:09 +0300

    added PDFComparator

--- src/PDFComparator/PDFComparator.java
+++ src/PDFComparator/PDFComparator.java
@@ -1,5 +1,26 @@
 package PDFComparator;
 
-public class PDFComparator {
+import PDFManager.PDFManager;
 
+public class PDFComparator {
+    private PDFManager firstPDFManager;
+    private PDFManager secondPDFManager;
+
+    public PDFComparator(PDFManager firstPDFManager, PDFManager secondPDFManager) {
+        this.firstPDFManager = firstPDFManager;
+        this.secondPDFManager = secondPDFManager;
+    }
+
+    public PDFManager getFirstPDFManager() {
+        return firstPDFManager;
+    }
+
+    public PDFManager getSecondPDFManager() {
+        return secondPDFManager;
+    }
+
+    //TODO: Multithreading open the pdf and write them to txt
+    public void compareTwoPDFs(String pathToFirstPDF, String PathToSecondPDF) {
+
+    }
 }
---
+++ src/PDFComparator/PDFComparatorTest.java
@@ -0,0 +1,11 @@
+package PDFComparator;
+
+public class PDFComparatorTest {
+
+    public static final String PATH_TO_FIRST_PDF = "as";
+    public static final String PATH_TO_SECOND_PDF = "as";
+
+    public static void main(String[] args) {
+
+    }
+}
